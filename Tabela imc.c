2744f64f361bf90cc2d19089adc6d98bccc64672
FelipeMPinheiro <felipe.pinheiro@hotmail.com>
2022-08-15 19:43:25 -0300

    programa que calcula o IMC do usuarioi

---
+++ Tabela imc.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+main(){
+	float peso, altura, imc;
+		printf("Qual seu peso?");
+		scanf("%f",&peso);
+		printf("Qual seu altura?");
+		scanf("%f",&altura);
+		imc=peso/altura
+	}
+
